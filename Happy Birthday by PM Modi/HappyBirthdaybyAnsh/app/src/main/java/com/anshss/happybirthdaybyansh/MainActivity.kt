e803425cf90392baa5641132cfd91265f6f03c8f
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2022-08-03 19:52:53 +0530

    Add files via upload

---
+++ Happy Birthday by PM Modi/HappyBirthdaybyAnsh/app/src/main/java/com/anshss/happybirthdaybyansh/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anshss.happybirthdaybyansh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
